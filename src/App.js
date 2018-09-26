cb20cc33b525f5a6789ba62811b5e50442064922
Brandon Schabel <brandonschabel1995@gmail.com>
2018-09-26 10:14:35 -0700

    add button to generate 4k image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Button from './components'
-import logo from './logo.svg';
+import GenButton from './components/GenButton'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button></Button>
+        <GenButton></GenButton>
       </div>
     );
   }
